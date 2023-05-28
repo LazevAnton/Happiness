e9cf4b48d6e160f5134a4413c20f97ccd60a425e
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-28 23:28:55 +0300

    Create models Teams and Members

--- HappinessAPP/models.py
+++ HappinessAPP/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Teams(models.Model):
+    team_name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return f'{self.team_name}'
+
+
+class Members(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    team = models.ForeignKey(Teams, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
