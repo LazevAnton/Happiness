8955b2ab14d034122ca8d862d6e849b115bfa9d7
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-29 14:47:51 +0300

    Create TeamsViewSet and MembersViewSet

--- HappinessAPP/views.py
+++ HappinessAPP/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from HappinessAPP.models import Teams, Members
+from HappinessAPP.serializers import TeamSerializer, MemberSerializer
+
+
+class TeamsViewSet(ModelViewSet):
+    queryset = Teams.objects.all()
+    serializer_class = TeamSerializer
+
+
+class MembersViewSet(ModelViewSet):
+    queryset = Members.objects.all()
+    serializer_class = MemberSerializer
