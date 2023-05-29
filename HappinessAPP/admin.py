5d62e76b6907fc6adfe3c50fb15e79a060add0b2
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-29 15:12:25 +0300

    PEP8

--- HappinessAPP/admin.py
+++ HappinessAPP/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from HappinessAPP.models import Members, Teams, HappinessLevel
+from HappinessAPP.models import Members, Teams
 
 admin.site.register([Teams, Members])
